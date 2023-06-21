a7e84eb521cd7110172a04d39932a7c6cc18867b
GaldinoAllan <galdinoallancodes@gmail.com>
2023-06-21 19:30:50 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Allan Galdino on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
